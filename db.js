1d1f028e56e9e953bbc94e90cc8fcc427de329c7
Puneet <puneet@gmail.com>
2026-02-13 17:29:31 +0530

    done

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql2');
+require("dotenv").config();
+const mysql = require("mysql2");
 
-// Connection create करो
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',             // तुम्हारा MySQL user
-  password: 'Root@978',// तुम्हारा root password
-  database: 'jewellery'     // project के हिसाब से database
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
-// Connect करो
 connection.connect((err) => {
   if (err) {
-    console.error('Connection failed:', err);
+    console.error("❌ Connection failed:", err);
   } else {
-    console.log('✅ MySQL Connected!');
+    console.log("✅ MySQL Connected!");
   }
 });
 
