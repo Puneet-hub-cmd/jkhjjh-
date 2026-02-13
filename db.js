c48b526e4209f056896d27dc29f970ac879413cc
Puneet <puneet@gmail.com>
2026-02-13 19:44:20 +0530

    donee

--- db.js
+++ db.js
@@ -1,19 +1,20 @@
-require("dotenv").config();
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
+// Connection create karo
 const connection = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT || 3306
 });
 
+// Connect karo
 connection.connect((err) => {
   if (err) {
-    console.error("❌ Connection failed:", err);
+    console.error('Connection failed:', err);
   } else {
-    console.log("✅ MySQL Connected!");
+    console.log('✅ MySQL Connected!');
   }
 });
 
